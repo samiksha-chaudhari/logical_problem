ad992424a98088cf975db33bfd9d1b95f1a20bbd
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 15:57:48 +0530

    prime number

--- logical_problem/Program.cs
+++ logical_problem/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace logical_problem
+namespace PRIME
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("logical problems");
+            Console.WriteLine("Enter number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            int count = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                    count++;
+            }
+
+            if (count == 2)
+                Console.Write("{0} is a prime number", num);
+            else
+                Console.Write("{0} is not a prime number", num);
+            Console.ReadLine();
         }
     }
 }
