9d7a1f4caae8c36cde2517a847aa2228faa90440
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 15:49:39 +0530

    perfect number

--- logical_problem/Program.cs
+++ logical_problem/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace logical_problem
+namespace myApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("logical problems");
+            Console.WriteLine("Enter number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            int i = 1;
+            int total = 0;
+
+            for (i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    total += i;
+            }
+            if (total == num)
+                Console.Write("{0} is a perfect number", num);
+            else
+                Console.Write("{0} is not a perfect number", num);
+            Console.ReadLine();
         }
     }
 }
