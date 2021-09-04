d291b905f2ed85a75b3d6c307d189de7576b6ce0
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 15:33:34 +0530

    logical problems

---
+++ logical_problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace logical_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("logical problems");
+        }
+    }
+}
