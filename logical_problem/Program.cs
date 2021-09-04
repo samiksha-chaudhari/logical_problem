75f1e95bb7601e9c146d65fd7221ea65f98dd9bf
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 15:43:23 +0530

    fibonacci series

--- logical_problem/Program.cs
+++ logical_problem/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace logical_problem
+namespace myApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("logical problems");
+            Console.WriteLine("Enter number up to fibonacci series require:");
+            int limit = Convert.ToInt32(Console.ReadLine());
+           
+            int a = 0;
+            int b = 1;
+            int c = a + b;
+
+            Console.WriteLine("Fibonacci series upto {0}: ", limit);
+            Console.Write("{0} {1} ", a, b);
+
+            while (c <= limit)
+            {
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+                c = a + b;
+            }
         }
     }
 }
