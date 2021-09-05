6cefabf3909bc2d1ced5585e2929a4175eda4c1c
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-05 23:23:39 +0530

    coupon number

--- logical_problem/Program.cs
+++ logical_problem/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
-namespace logical_problem
+namespace COUPON
 {
     class Program
     {
+        static void Coupon(int n)
+        {
+            int[] rand = new int[n];
+            int[] distict = new int[n];
+            for (int i = 1; i < n; i++)
+            {
+                Random random = new Random();
+                int num = random.Next(1, 100);
+                rand[i] = num;
+            }
+            Console.WriteLine("distinct coupon numbers are :");
+            int a = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int count = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (rand[i] == rand[j])
+                    {
+                        count++;
+                    }
+
+                }
+                distict[a] = count;
+                a++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+              Console.WriteLine(rand[i]);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("logical problems");
+            
+            int n;
+            Console.Write("Enter the number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Coupon(n);
         }
     }
 }
