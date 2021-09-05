d4d3aadbf6cf7a3a49c1702d0e1fc294c8d1181d
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-05 22:55:42 +0530

    elapse time

--- logical_problem/Program.cs
+++ logical_problem/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
-namespace logical_problem
+using System.Diagnostics; // to use Stopwatch feature
+using System.Threading; // to perform multiple operations
+class Demo_Stopwatch // Demo class for implementing the Stopwatch functionality
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Stopwatch stopw = new Stopwatch();
+        stopw.Start();
+        for (int i = 0; i < 100; i++)
         {
-            Console.WriteLine("logical problems");
+            Thread.Sleep(2);
         }
+        stopw.Stop();
+        Console.WriteLine(" Time elapsed: {0} ", stopw.Elapsed);
     }
 }
